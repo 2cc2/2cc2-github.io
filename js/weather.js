2f6133379a6774d84e3252e0166342a6851ee406
2cc2 <2580272045@qq.com>
2023-05-01 18:56:00 +0800

    Site updated: 2023-05-01 18:56:00

---
+++ js/weather.js
@@ -0,0 +1,25 @@
+
+WIDGET = {
+  "CONFIG": {
+    "modules": "4201",
+    "background": "4",
+    "tmpColor": "B4A7D6",
+    "tmpSize": "16",
+    "cityColor": "B6D7A8",
+    "citySize": "16",
+    "aqiColor": "CFE2F3",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "1",
+    "language": "auto",
+    "borderRadius": "10",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "left": "145",
+    "top": "10",
+    "key": "3bfd90bc2194425eb686218206db2e12"
+  }
+}
